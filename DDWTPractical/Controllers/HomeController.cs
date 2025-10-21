8e693fe985ca48e92e06919e594cdc185234e655
Priiy005 <priiy005@mymail.unisa.edu.au>
2025-10-21 14:21:19 +1030

    Add project files.

---
+++ DDWTPractical/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DDWTPractical.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home/ or Home/Index
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+        // GET: Home/ or Home/Details
+        public ActionResult Details()
+        {
+            ViewBag.Title = "Product";
+            return View();
+        }
+
+        // GET: Home/About
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            ViewBag.Message = "About Amazon Orders...";
+
+            return View();
+        }
+
+        // GET: Home/Contact
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+            ViewBag.Message = "Contact Amazon Orders...";
+
+            return View();
+        }
+    }
+}
---
+++ DDWTPractical/Controllers/ManagementController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace DDWTPractical.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management or /Management/Index
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Manage Products";
+            return View();
+        }
+
+        // GET: Management/Details
+        public ActionResult Details()
+        {
+            ViewBag.Title = "View Product";
+            return View();
+        }
+
+        // GET: Management/Create
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Create Product";
+            return View();
+        }
+
+        // GET: Management/Edit 
+        public ActionResult Edit()
+        {
+            ViewBag.Title = "Edit Product";
+            return View();
+        }
+
+        // GET: Management/Delete 
+        public ActionResult Delete()
+        {
+            ViewBag.Title = "Delete Product";
+            return View();
+        }
+    }
+}
